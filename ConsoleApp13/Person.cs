64b7195aaeec5dd6d9fe079e0aa425a66ad7bd95
boronin_1993@mail.ru <boronin_1993@mail.ru>
2022-12-24 13:15:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Person.cs
@@ -0,0 +1,34 @@
+﻿
+using static System.Console;
+
+namespace ConsoleApp13
+{
+    class Person
+    {
+        public string name ;
+        //public Company company ;
+        public int age;
+
+        //public Person(): this("Неизвестно") { }
+        //public Person(string name): this(name,18) { }
+        public Person(string name="Неизвестно", int age=18) 
+        { 
+            this.name =name;
+            this.age = age; 
+            //company = new Company(); 
+        }
+        public void Deconstruct(out string personName, out int personAge)
+        {
+            personAge = age;
+            personName = name;
+        }
+        public void Print()
+        {
+            WriteLine($"Имя: {name} Возраст: {age}");
+        }
+    }
+    //class Company
+    //{
+    //    public string title="Неизвестно";
+    //}
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,55 @@
+﻿using ConsoleApp13;
+using static System.Console;
+
+/*//Person person = new Person();
+
+//string personName = person.name;
+//int personAge=person.age;
+//WriteLine($"Имя: {personName} Возраст: {personAge}");
+
+//person.name = "Ted";
+//person.Print();
+//person.age = 30;
+//Person person1 = new Person { name = "Ted" };
+//Person person2 = new Person { name = "Bred", age = 25 };
+//person1.Print();
+//person2.Print();
+//(_, int age) = person2;
+//WriteLine( " " + age);
+
+//person.Print();*/
+/*Person person=new Person();
+person.age=30;
+person.name="Bred";
+person.Print();
+Person person1 = person with {name="Bod"};
+person1.Print();
+struct Person
+{
+    public string name;
+    public int age;
+    public Person() : this("Неизвестно") { }
+    public Person(string name):this(name, 18) { }
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Print()
+    {
+        WriteLine($"Имя: {name} Возраст {age}");
+    }
+
+}*/
+Calculate(5);
+static void Calculate(int t)
+{
+    object x = 6;
+    int y = 7;
+    int z = y + t;
+}
+
+
+
+
